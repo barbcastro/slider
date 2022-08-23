aa11379e7f9031e41518929809875f5d42883e27
Barbara <barbara_castrosilva@hotmail.com>
2022-08-23 19:47:46 -0300

    slider inclusão do JS

---
+++ index.js
@@ -0,0 +1,67 @@
+const previousEl = document.getElementById('previous');
+const nextEl = document.getElementById('next');
+const sliderEl = document.getElementById('slider');
+let interval = undefined;
+let setTimeout = undefined;
+let selectedImgIndex = 0;
+
+previousEl.addEventListener('click', onPreviousClick);
+nextEl.addEventListener('click', onNextClick);
+
+autoScroll();
+
+function onPreviousClick() {
+  const sliderWidth = sliderEl.offsetWidth;
+  sliderEl.scrollLeft -= sliderWidth;
+  --selectedImgIndex
+  handleActiveDot();
+  handSliderClick();
+}
+
+function onNextClick() {
+  const sliderWidth = sliderEl.offsetWidth;
+  sliderEl.scrollLeft += sliderWidth;
+  ++selectedImgIndex
+  handleActiveDot();
+  handSliderClick();
+}
+
+function handSliderClick() {
+    clearTimeout(timeout);
+    clearInterval(interval);
+    interval =undefined;
+  setTimeout( ()=> {
+    autoScroll ();
+  }, 30000);
+}
+
+function handleActiveDot() {
+   const list = Array.from(document.getElementsByClassName('dot')); 
+  
+   if(selectedImgIndex < 0) selectedImgIndex = 0;
+   if(selectedImgIndex >= list.length) selectedImgIndex = list.length -1;
+
+  list.forEach(el => el.classList.remove('active'));
+  list[selectedImgIndex].classList.add('active');
+}
+
+function autoScroll() {
+  if(interval) return;
+
+  interval = setInterval(()=> {
+    const sliderWidth = sliderEl.offsetWidth;
+    const numberOfImages =sliderEl.childElementCount;
+    const selectedImage = (sliderEl.scrollLeft/sliderWidth) + 1;
+
+    if(numberOfImages === selectedImage) {
+        sliderEl.scrollLeft = 0;
+        selectedImgIndex = 0;
+        handleActiveDot();
+        return;
+    }
+  
+    sliderEl.scrollLeft += sliderWidth;
+    ++selectedImgIndex;
+    handleActiveDot();
+  }, 7000);
+}
